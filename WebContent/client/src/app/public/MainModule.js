c7564da4aa5cd8dec488230efea62e19813a0ba8
gwnio <mrg212@hotmail.com>
2018-01-14 15:12:48 -0500

    Removed signup module and placed in public main module

--- WebContent/client/src/app/public/MainModule.js
+++ WebContent/client/src/app/public/MainModule.js
@@ -3,20 +3,28 @@ define([
 	'src/app/widget/PasswordValidCheckDirective',
 	'src/app/public/MainController',
 	'src/app/public/login/LoginController',
-	'src/app/public/qsignup/SignupModule'
+	'src/app/public/signup/SignupUniqueEmailCheckDirective',
+	'src/app/public/signup/SignupModel',
+	'src/app/public/qsignup/SignupController'
 ], function(
 	ValuesEqualRequiredDirective,
 	PasswordValidCheckDirective,
 	MainController,
 	LoginController,
-	SignupModule) {
-	var module = angular.module('app.login', ['ngMessages', 'app.signup']);
+	SignupUniqueEmailCheckDirective,
+	SignupModel,
+	SignupController) {
+	var module = angular.module('app.login', ['ngMessages']);
 	
 	module.directive('valuesEqualRequired', ValuesEqualRequiredDirective);
 	module.directive('passwordValidCheck', PasswordValidCheckDirective);
+	module.directive('signupUniqueEmailCheck', SignupUniqueEmailCheckDirective);
+	
+	module.factory('SignupModel', SignupModel);
 	
 	module.controller('PublicMainController', MainController);
 	module.controller('LoginController', LoginController);
+	module.controller('SignupController', SignupController);
 	
 	module.config(['$stateProvider', function($stateProvider) {
 		$stateProvider
@@ -30,6 +38,12 @@ define([
 			url : '/login?go',
 			templateUrl : 'client/src/app/public/login/login.tpl.html',
 			controller : 'LoginController',
+			controllerAs : 'ctrlr'
+		})
+		.state('app.signup', {
+			url : '/signup',
+			templateUrl : 'client/src/app/public/qsignup/signup.tpl.html',
+			controller : 'SignupController',
 			controllerAs : 'ctrlr'
 		});
 	}]);
